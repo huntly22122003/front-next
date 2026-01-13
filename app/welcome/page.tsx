49e4316701373473690df1c46440efdc89adc425
LyHung <huntly1306@gmail.com>
2026-01-13 09:13:23 +0700

    Check Backend

---
+++ app/welcome/page.tsx
@@ -0,0 +1,30 @@
+"use client";
+import { useEffect, useState } from "react";
+
+export default function WelcomePage() {
+  const [msg, setMsg] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:8080/hello")
+      .then(async (res) => {
+        const text = await res.text();
+        console.log("Raw response:", text);
+
+        try {
+          const data = JSON.parse(text); // parse JSON nếu đúng
+          setMsg(data.message);
+        } catch (e) {
+          // nếu không phải JSON thì hiển thị raw text
+          setMsg(text);
+        }
+      })
+      .catch((err) => console.error("API error:", err));
+  }, []);
+
+  return (
+    <main style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>🎉 Welcome to Next.js 16!</h1>
+      <p>Laravel backend trả về: {msg}</p>
+    </main>
+  );
+}
